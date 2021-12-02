f3792bad1a85589d8500e85ead094a902f0f2765
Felipe <jflobom@gmail.com>
2021-12-02 01:27:11 -0500

    Add app name

--- pages/playlists.tsx
+++ pages/playlists.tsx
@@ -1,27 +1,29 @@
-import {useEffect, useState} from "react";
-import {Playlist} from "../application/models/playlists/Playlist";
+import { useEffect, useState } from "react";
+import { Playlist } from "../application/models/playlists/Playlist";
 import PlaylistsManager from "../application/managers/PlaylistsManager";
 import PrivateLayout from "../shared/components/PrivateLayout";
-import {Button} from "../shared/components/Button/Button";
+import { Button } from "../shared/components/Button/Button";
 
-export default function Playlists(){
-    const [playlists, setPlaylists] = useState<Playlist[]>([])
+export default function Playlists() {
+  const [playlists, setPlaylists] = useState<Playlist[]>([]);
 
-    useEffect(() => {
-        fetchUserPlaylists()
-    }, [])
+  useEffect(() => {
+    fetchUserPlaylists();
+  }, []);
 
-    const fetchUserPlaylists = async () => {
-       const userPlaylists = await PlaylistsManager.getUserPlaylists()
-        if(userPlaylists.length){
-            setPlaylists(userPlaylists)
-        }
+  const fetchUserPlaylists = async () => {
+    const userPlaylists = await PlaylistsManager.getUserPlaylists();
+    if (userPlaylists.length) {
+      setPlaylists(userPlaylists);
     }
-    return (
-        <PrivateLayout>
-            <h1>Playlists...</h1>
-            <Button dataCy="button" type="primary" text="Hola" />
-            {playlists.map((item: any) => <p key={item.uri}>{item.name}</p>)}
-        </PrivateLayout>
-    )
+  };
+  return (
+    <PrivateLayout>
+      <h1>Playlists...</h1>
+      <Button dataCy="button" type="primary" text="Hola" />
+      {playlists.map((item: any) => (
+        <p key={item.uri}>{item.name}</p>
+      ))}
+    </PrivateLayout>
+  );
 }
